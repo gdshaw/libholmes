29d26eaf4dbdb212381a877df52b844b4fcf18dd
Graham Shaw <gdshaw@riscpkg.org>
2021-04-27 20:30:00 +0100

    Added holmes command.

--- Makefile
+++ Makefile
@@ -5,15 +5,29 @@
 
 prefix = /usr/local
 exec_prefix = $(prefix)
+bindir = $(exec_prefix)/bin
 libdir = $(exec_prefix)/lib
+libexecdir = $(libdir)
 
-CPPFLAGS = -MD -MP -I.
+pkgname = holmes
+
+CPPFLAGS = -MD -MP -I. '-DLIBEXECDIR="$(libexecdir)"' '-DPKGNAME="$(pkgname)"'
 CXXFLAGS = -fPIC -O2 --std=c++17 -Wall -Wpedantic
+LDLIBS = -ldl
+
+SRC = $(wildcard src/*.cc)
+BIN = $(SRC:src/%.cc=bin/%)
+MAINBIN = bin/$(pkgname)
+AUXBIN = $(filter-out $(MAINBIN),$(BIN))
 
 HOLMES = $(wildcard holmes/*.cc) $(wildcard holmes/*/*.cc) $(wildcard holmes/*/*/*.cc)
 
 .PHONY: all
-all: holmes.so
+all: $(BIN)
+
+$(BIN): bin/%: src/%.o holmes.so
+	@mkdir -p bin
+	g++ -rdynamic -Wl,-rpath $(libdir) -o $@ $^ $(LDLIBS)
 
 holmes.so: $(HOLMES:%.cc=%.o)
 	gcc -shared -o $@ $^
@@ -23,15 +37,23 @@ clean:
 	rm -f holmes/*.[do]
 	rm -f holmes/*/*.[do]
 	rm -f holmes/*/*/*.[do]
+	rm -f src/*.[do]
 	rm -f *.so
+	rm -rf bin
 
 .PHONY: install
 install: all
+	@mkdir -p $(bindir)
 	@mkdir -p $(libdir)
+	@mkdir -p $(libexecdir)/$(pkgname)/bin
+	cp $(MAINBIN) $(bindir)/
 	cp holmes.so $(libdir)/
+	cp $(AUXBIN) $(libexecdir)/$(pkgname)/bin/
 
 .PHONY: uninstall
 uninstall:
-	rm -f $(libdir)/holmes.so
+	rm -f $(bindir)/$(notdir $(MAINBIN))
+	rm -rf $(libexecdir)/$(pkgname)
 
 -include $(HOLMES:%.cc=%.d)
+-include $(SRC:%.cc=%.d)
---
+++ src/holmes.cc
@@ -0,0 +1,116 @@
+// This file is part of libholmes.
+// Copyright 2021 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+#include <cerrno>
+#include <climits>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+#include <unistd.h>
+#include <getopt.h>
+
+/** Print help text.
+ * @param out the ostream to which the help text should be written
+ */
+void write_help(std::ostream& out) {
+	out << "Usage: holmes [<options>] command [<args>]" << std::endl;
+	out << std::endl;
+	out << "Options:" << std::endl;
+	out << std::endl;
+	out << "  -h  display this help text then exit" << std::endl;
+	out << "  -V  display version number then exit" << std::endl;
+}
+
+/** Print version information.
+ * @param out the ostream to which the version information should be written.
+ */
+void write_version(std::ostream& out) {
+	out << "libholmes (experimental version)" << std::endl;
+}
+
+extern char** environ;
+
+int main(int argc, char* argv[]) {
+	// Parse command line options.
+	int opt;
+	while ((opt = getopt(argc, argv, "+hV")) != -1) {
+		switch (opt) {
+		case 'h':
+			write_help(std::cout);
+			return 0;
+		case 'V':
+			write_version(std::cout);
+			return 0;
+		}
+	}
+
+	// Error if no command given.
+	if (optind == argc) {
+		std::cerr << "Command not specified. See holmes -h."
+			<< std::endl;
+		std::exit(1);
+	}
+
+	// The command name will later be interpolated into a pathname.
+	// There is unlikely to be any requirement to support commands
+	// containing slash characters, therefore they are forbidden
+	// to avoid unexpected behaviour.
+	const char* cmdname = argv[optind];
+	if (strchr(cmdname, '/')) {
+		std::cerr << "Command not recognised. See holmes -h."
+			<< std::endl;
+		std::exit(1);
+	}
+
+	// Construct the pathname for the executable corresponding to command.
+	// The filename is also required, for use as what will become argv[0].
+	// This is done by recording the index into the pathname at
+	// which the filename begins.
+	const char* cmdprefix = "holmes-";
+	char pathname[PATH_MAX];
+	int filename_idx = 0;
+	ssize_t count = snprintf(pathname, sizeof(pathname), "%s/%s/bin/%n%s%s",
+		LIBEXECDIR, PKGNAME, &filename_idx, cmdprefix, cmdname);
+	if (count < 0) {
+		perror("Failed to construct command");
+		std::exit(1);
+	} else if (static_cast<size_t>(count) >= sizeof(pathname)) {
+		std::cerr << "Command too long." << std::endl;
+		std::exit(1);
+	}
+
+	// The arguments passed to the command will be argv[optind]
+	// onwards. The first argument should be a filename string
+	// associated with the command to be executed, but it currently
+	// contains only the raw command name. Change it to point to
+	// the filename component within the constructed pathname.
+	argv[optind] = pathname + filename_idx;
+
+	// Attemt to invoke the executable at the constructed pathname.
+	if (execve(pathname, argv + optind, environ) == -1) {
+		// Want to give user-friendly error message for the case
+		// where there is no executable corresponding to the given
+		// command name. This would result in errno being set to
+		// ENOENT, but is not the only way that could happen.
+		// Therefore, if execve fails for any reason, try testing
+		// whether the executable exists before deciding on how
+		// to report the error.
+		int execve_errno = errno;
+		if ((access(pathname, F_OK) == -1) && (errno == ENOENT)) {
+			std::cerr << "Command not recognised. See holmes -h."
+				<< std::endl;
+			std::exit(1);
+		} else {
+			errno = execve_errno;
+			perror("Command execution failed");
+			std::exit(1);
+		}
+	}
+
+	// This statement should be unreachable, but if it were reached then
+	// that would indicate that an error had occurred.
+	return 1;
+}
